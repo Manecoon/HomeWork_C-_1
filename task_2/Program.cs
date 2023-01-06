71569589e0ab69d527333c6c51da794fdc540508
Manecoon <stas_on.85@mail.ru>
2023-01-06 12:42:10 +0300

    Task_2

---
+++ task_2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Программа приниает на вход три числа и выдает максимальное из этих чисел.
+Console.WriteLine("Веедите первое число: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Веедите второе число: ");
+int numB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Веедите третье число: ");
+int numC = Convert.ToInt32(Console.ReadLine());
+int max = numA;
+if (numA > max) max = numA;
+if (numB > max) max = numB;
+if (numC > max) max = numC;
+Console.Write("Максимальное число: ");
+Console.Write(max);
