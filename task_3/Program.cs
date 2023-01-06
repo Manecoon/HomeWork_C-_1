b7abd1923e7017866a377caa6cdbb37349686ef1
Manecoon <stas_on.85@mail.ru>
2023-01-06 13:05:36 +0300

    Task_3

---
+++ task_3/Program.cs
@@ -0,0 +1,7 @@
+﻿// Программа принимает на вход число и выдает, яыляется ли число четным.
+Console.WriteLine("Веедите число: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+if (numA % 2 == 0) 
+Console.Write($"Число {numA} является четным.");
+else 
+Console.Write($"Число {numA} является НЕчетным.");
