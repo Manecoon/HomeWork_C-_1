abf158dfa2950b45b17ac697a17ccbd74db46ef4
Manecoon <stas_on.85@mail.ru>
2023-01-06 12:28:17 +0300

    Tak_1

---
+++ task_1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Программа, которая на вход принимает два числа и выдает, какое число большее, а какое меньшее.
+
+Console.WriteLine("Веедите первое число: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Веедите второе число: ");
+int numB = Convert.ToInt32(Console.ReadLine());
+if (numA > numB)
+{
+    Console.Write("Наибольшее число: ");
+    Console.Write(numA);
+}
+else
+{
+    Console.Write("Наибольшее число: ");
+    Console.Write(numB);
+}
