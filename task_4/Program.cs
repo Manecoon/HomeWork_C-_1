22e4970f296441bd975ab2577d3218bdcf38d641
Manecoon <stas_on.85@mail.ru>
2023-01-06 14:19:35 +0300

    Task_4

---
+++ task_4/Program.cs
@@ -0,0 +1,14 @@
+﻿// Программа на вход принимает число (N), а на выходе показывает все четные числа от 1 до N.
+Console.WriteLine("Веедите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int index = 1;
+Console.Write($"Все четные числа до числа {N}:");
+while (index <= N)
+{
+    if (index % 2 == 0)
+    {
+        Console.Write($" {index}");
+        index++;
+    }
+    index++;
+}
